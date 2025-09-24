b692fbd06fc6a7a050376dd973f8245847ce0f39
loay alzaben <loayalzaben123@gmail.com>
2025-09-24 14:00:51 +0300

    chore(settings): robust parsing for DJANGO_ALLOWED_HOSTS (filter empty)

--- lawyer_site/settings.py
+++ lawyer_site/settings.py
@@ -10,7 +10,8 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'change-me')
 
 DEBUG = os.getenv('DJANGO_DEBUG', '1') == '1'
 
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost 127.0.0.1').split()
+_hosts = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost 127.0.0.1')
+ALLOWED_HOSTS = [h for h in _hosts.split() if h]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
